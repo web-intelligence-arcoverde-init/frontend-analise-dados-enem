e3f3b90b007202116f91a0d943778ce157488362
ramonlopz1 <davidramon123@hotmail.com>
2023-01-27 17:47:08 -0300

    create conditional css to apply colors on table td

--- src/components/PageHeatMap/organisms/HeatTable/HeatTable.tsx
+++ src/components/PageHeatMap/organisms/HeatTable/HeatTable.tsx
@@ -6,7 +6,16 @@ const fakeData = [
     nQuestions: 2,
     nHits: '33%',
     errors: 4,
-    nPercentHits: '5%',
+    nPercentHits: '51.3%',
+    nPercentNordeste: '23.8%',
+  },
+  {
+    habilities: 1,
+    nQuestions: 2,
+    nHits: '33%',
+    errors: 4,
+    nPercentHits: '35.8%',
+    nPercentNordeste: '23%',
   },
   {
     habilities: 1,
@@ -14,6 +23,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '19%',
   },
   {
     habilities: 1,
@@ -21,6 +31,23 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
+  },
+  {
+    habilities: 1,
+    nQuestions: 2,
+    nHits: '33%',
+    errors: 4,
+    nPercentHits: '15%',
+    nPercentNordeste: '29%',
+  },
+  {
+    habilities: 1,
+    nQuestions: 2,
+    nHits: '33%',
+    errors: 4,
+    nPercentHits: '30%',
+    nPercentNordeste: '40%',
   },
   {
     habilities: 1,
@@ -28,6 +55,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -35,6 +63,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -42,6 +71,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -49,6 +79,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -56,6 +87,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -63,6 +95,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -70,6 +103,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -77,6 +111,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -84,6 +119,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -91,6 +127,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -98,6 +135,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -105,6 +143,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -112,6 +151,7 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -119,6 +159,15 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
+  },
+  {
+    habilities: 1,
+    nQuestions: 2,
+    nHits: '33%',
+    errors: 4,
+    nPercentHits: '35%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
@@ -126,65 +175,70 @@ const fakeData = [
     nHits: '33%',
     errors: 4,
     nPercentHits: '5%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
     nQuestions: 2,
     nHits: '33%',
     errors: 4,
-    nPercentHits: '5%',
+    nPercentHits: '8%',
+    nPercentNordeste: '49%',
   },
   {
     habilities: 1,
     nQuestions: 2,
     nHits: '33%',
     errors: 4,
-    nPercentHits: '5%',
-  },
-  {
-    habilities: 1,
-    nQuestions: 2,
-    nHits: '33%',
-    errors: 4,
-    nPercentHits: '5%',
-  },
-  {
-    habilities: 1,
-    nQuestions: 2,
-    nHits: '33%',
-    errors: 4,
-    nPercentHits: '5%',
-  },
-  {
-    habilities: 1,
-    nQuestions: 2,
-    nHits: '33%',
-    errors: 4,
-    nPercentHits: '5%',
-  },
-  {
-    habilities: 1,
-    nQuestions: 2,
-    nHits: '33%',
-    errors: 4,
-    nPercentHits: '5%',
+    nPercentHits: '23%',
+    nPercentNordeste: '29%',
   },
 ]
 
 export const HeatTable = () => {
+  const applyColor = (value: string) => {
+    // essa função aplica a cor da célual da tabela
+
+    // remove %, troca , por . e formata para float (pois é uma string)
+    const formatedValue = parseFloat(
+      value.replace('%', '').replace(",", ".")
+    )
+
+    const heatColors = [
+      { color: '#ffcdd2', min: 40, max: 54 },
+      { color: '#ef9a9a', min: 30, max: 40 },
+      { color: '#e57373', min: 20, max: 30 },
+      { color: '#e53935', min: 10, max: 20 },
+      { color: '#ea0909', min: 0, max: 10},
+    ]
+
+    let color
+    for(let i = 0; i < heatColors.length; i++) {
+      if (formatedValue >= heatColors[i].min && formatedValue <= heatColors[i].max) {
+        color = heatColors[i].color
+        break
+      } else {
+        color = 'white'
+      }
+    }
+
+    return color
+  }
+
   return (
     <div className={styles.wrapper}>
       <table className={styles.table}>
         <thead>
           <tr className="tdRow">
             <th rowSpan={3}>Habilidades</th>
-            <th colSpan={3}>Linguagens, códigos e suas tecnologias</th>
+            <th colSpan={4}>Linguagens, códigos e suas tecnologias</th>
             <th>Nordeste</th>
           </tr>
           <tr>
             <th>Nº Questões</th>
             <th>Acerto</th>
             <th>Erros</th>
+            <th>% de Acerto</th>
             <th>% de Acerto</th>
           </tr>
         </thead>
@@ -197,7 +251,20 @@ export const HeatTable = () => {
                   <td>{td.nQuestions}</td>
                   <td>{td.nHits}</td>
                   <td>{td.errors}</td>
-                  <td>{td.nPercentHits}</td>
+                  <td
+                    style={{
+                      backgroundColor: `${applyColor(td.nPercentHits)}`,
+                    }}
+                  >
+                    {td.nPercentHits}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor: `${applyColor(td.nPercentNordeste)}`,
+                    }}
+                  >
+                    {td.nPercentNordeste}
+                  </td>
                 </tr>
               )
             } else {
